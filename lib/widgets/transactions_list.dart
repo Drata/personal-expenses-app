22ec4b12eb54f619eec4e04597ee33319eb982f9
antonio.diaz <antonio.diaz@meteologica.com>
2019-11-20 08:39:56 +0100

    const & transction_item

--- lib/widgets/transactions_list.dart
+++ lib/widgets/transactions_list.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import '../models/transaction.dart';
-import 'package:intl/intl.dart';
+import './transaction_item.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
@@ -16,41 +16,15 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return transactions.isEmpty
         ? Center(
-            child: Text('No transactions added yet.'),
+            child: const Text('No transactions added yet.'),
           )
         : ListView.builder(
             itemCount: transactions.length,
             itemBuilder: (ctx, index) {
-              return Card(
-                margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
-                elevation: 5,
-                child: ListTile(
-                  leading: CircleAvatar(
-                    backgroundColor: Theme.of(context).primaryColor,
-                    radius: 30,
-                    child: Padding(
-                      padding: EdgeInsets.all(6),
-                      child: FittedBox(
-                          child: Text('${transactions[index].amount}\€')),
-                    ),
-                  ),
-                  title: Text('${transactions[index].title}'),
-                  subtitle:
-                      Text(DateFormat.yMMMd().format(transactions[index].date)),
-                  trailing: MediaQuery.of(context).size.width >= 500
-                      ? FlatButton.icon(
-                          label: Text('Delete'),
-                          textColor: Colors.red,
-                          icon: Icon(Icons.delete),
-                          onPressed: () => deleteTx(transactions[index].id),
-                        )
-                      : IconButton(
-                          icon: Icon(Icons.delete),
-                          color: Colors.red,
-                          onPressed: () => deleteTx(transactions[index].id)),
-                ),
-              );
+              return TransactionItem(transaction: transactions[index], deleteTx: deleteTx);
             },
           );
   }
 }
+
+
